b2bc0b4303d92f97b3897c6f47699ece459c75eb
unknown <parabatai2712@gmail.com>
2021-05-06 08:37:40 -0500

    Actualizacion de la interfaz grafica y el menu de juego

--- Programa.java
+++ Programa.java
@@ -4,5 +4,17 @@ public class Programa{
 		System.out.println(" / __) )( (  __) ___) ___|_  _) )( (  __) ___)/  \\(  ( \\/ __) ");
 		System.out.println("( (_ ) \\/ () _)\\___ \\___ \\ )( ) __ () _)\\___ (  O )    ( (_ \\ ");
 		System.out.println(" \\___|____(____|____(____/(__)\\_)(_(____|____/\\__/\\_)__)\\___/ " );
+		System.out.println("¡Hola!")
+	
+		System.out.println("¡Hola! Bienvenido a GuessTheSong, estas son las opciones de juego:");
+    	System.out.println("1. Ingrese un numero del 1-10");
+    	System.out.println("2. En caso de apoyo, presione 2 para ver un fragmento de la letra");
+    	System.out.println("3. Imprima las letras de las canciones");
+    	System.out.println("4. Ingrese el nombre de la cancion");
+    	System.out.println("5. salir del juego");
+	
 	}
+
+
+
 }
