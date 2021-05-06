a27c814c346ff415515ded3c4fb90f149a1d9632
unknown <parabatai2712@gmail.com>
2021-05-05 22:56:27 -0500

    Avance de la interfaz grafica

--- Programa.java
+++ Programa.java
@@ -1,5 +1,8 @@
 public class Programa{
 	public static void main(String[] args) {
-		
+		System.out.println("  ___ _  _ ____ ____ ____ ____ _  _ ____ ____  __  __ _  ___   ");
+		System.out.println(" / __) )( (  __) ___) ___|_  _) )( (  __) ___)/  \\(  ( \\/ __) ");
+		System.out.println("( (_ ) \\/ () _)\\___ \\___ \\ )( ) __ () _)\\___ (  O )    ( (_ \\ ");
+		System.out.println(" \\___|____(____|____(____/(__)\\_)(_(____|____/\\__/\\_)__)\\___/ " );
 	}
 }
