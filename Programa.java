0fd79e82f38553ba53f0ef3c561886b333006ff3
unknown <parabatai2712@gmail.com>
2021-05-13 08:50:14 -0500

    Avance de la interfaz grafica

--- Programa.java
+++ Programa.java
@@ -62,17 +62,23 @@ public class Programa{
     }
 
 	public static void main(String[] args) {
-		System.out.println("  ___ _  _ ____ ____ ____ ____ _  _ ____ ____  __  __ _  ___   ");
-		System.out.println(" / __) )( (  __) ___) ___|_  _) )( (  __) ___)/  \\(  ( \\/ __) ");
-		System.out.println("( (_ ) \\/ () _)\\___ \\___ \\ )( ) __ () _)\\___ (  O )    ( (_ \\ ");
-		System.out.println(" \\___|____(____|____(____/(__)\\_)(_(____|____/\\__/\\_)__)\\___/ " );
-        System.out.println("                        ======o     o====== " );
-        System.out.println("                            ___________      " );
-        System.out.println("                           |___________|     " );
-        System.out.println("                            | \\ /\\ /\\ |       ");
-        System.out.println("                            | _\\/__\\/_|       ");
-        System.out.println("                           |___________|      ");
-		
+		System.out.println("  ___ _  _ ____ ____ ____ ____ _  _ ____ ____  __  __ _  ___                    ");
+		System.out.println(" / __) )( (  __) ___) ___|_  _) )( (  __) ___)/  \\(  ( \\/ __)                 ");
+		System.out.println("( (_ ) \\/ () _)\\___ \\___ \\ )( ) __ () _)\\___ (  O )    ( (_ \\             ");
+		System.out.println(" \\___|____(____|____(____/(__)\\_)(_(____|____/\\__/\\_)__)\\___/              ");
+        System.out.println("  ,           ;            ======o     o======      ,              ;            ");
+        System.out.println("  ;;          ;;               ___________          ;;             ;;           ");
+        System.out.println("  ;;          ;';.            |___________|         ;;             ;';.         ");
+        System.out.println("  ;;          ;  ;;            | \\ /\\ /\\ |          ;;             ;  ;;     ");
+        System.out.println("  ;;          ;   ;;           | _\\/__\\/_|          ;;             ;   ;;     ");
+        System.out.println("  ;; .;;;,    ;    ;;         |___________|         ;; .;;;,       ;    ;;      ");
+        System.out.println("  ;;.'  ;;    ;    ;;                               ;;.'  ;;       ;    ;;      ");
+        System.out.println("  ;;' .;'     ;   ;'                                ;;' .;'        ;   ;'       ");
+        System.out.println("  ;;.;'       ;  '                                  ;;.;'          ;  '         ");
+        System.out.println("  ;;'    ,;;;,;                                     ;;'       ,;;;,;            ");
+        System.out.println("         ;;;;;;                                     ,'        ;;;;;;            ");
+        System.out.println("         `;;;;'                                               `;;;;'            ");
+        
         AnsiConsole.systemInstall();
     	Audio audio = new Audio();
         Random randomGenerator = new Random ();
